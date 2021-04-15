efa94953703da77232cddfa477ec44c1fd38ba7b
Miquel Angel <hulkrojo2@gmail.com>
2021-04-15 09:50:42 +0200

    Added join message method

--- src/main/java/es/hulk/survival/listeners/ChatListener.java
+++ src/main/java/es/hulk/survival/listeners/ChatListener.java
@@ -9,6 +9,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatListener implements Listener {
 
     private final FileConfig mainConfig = Survival.get().getMainConfig();
@@ -16,6 +19,12 @@ public class ChatListener implements Listener {
     @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
+
+        if (mainConfig.getBoolean("JOIN-MESSAGE.ENABLE")) {
+            mainConfig.getStringList("JOIN-MESSAGE.LINES").forEach(lines -> {
+                lines = Utils.color(PlaceholderAPI.setPlaceholders(player, lines));
+            });
+        }
 
         if (mainConfig.getBoolean("CHAT-FORMAT.ENABLE")) {
             event.setFormat(Utils.color(PlaceholderAPI.setPlaceholders(player, mainConfig.getString("CHAT-FORMAT.FORMAT").replace("%message%", Utils.color("%2$s")))));
