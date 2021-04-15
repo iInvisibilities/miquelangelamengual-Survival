bea5aa6476e1212cd7bbcea23f5bf2d6819e4eee
Miquel Angel <hulkrojo2@gmail.com>
2021-04-15 15:33:39 +0200

    Fixed the fucking tablist

--- src/main/java/es/hulk/survival/providers/TablistProvider.java
+++ src/main/java/es/hulk/survival/providers/TablistProvider.java
@@ -1,27 +1,34 @@
 package es.hulk.survival.providers;
 
+import dev.risas.panda.files.FileConfig;
 import es.hulk.survival.Survival;
-import es.hulk.survival.utils.FileConfig;
 import es.hulk.survival.utils.Utils;
 import io.github.nosequel.tab.shared.entry.TabElement;
 import io.github.nosequel.tab.shared.entry.TabElementHandler;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.entity.Player;
 
-public class TablistProvider implements TabElementHandler {
+import java.util.Arrays;
+import java.util.List;
 
-    private final FileConfig tablistConfig = Survival.get().getTablistConfig();
+public class TablistProvider implements TabElementHandler {
 
     @Override
     public TabElement getElement(Player player) {
         final TabElement element = new TabElement();
-/*
-        element.setHeader(Utils.color(PlaceholderAPI.setPlaceholders(player, tablistConfig.getString("TABLIST.HEADER"))));
-        element.setFooter(Utils.color(PlaceholderAPI.setPlaceholders(player, tablistConfig.getString("TABLIST.FOOTER"))));
-*/
 
-        element.setFooter("Footer");
-        element.setHeader("Header");
+        List<String> test = Arrays.asList("LEFT", "MIDDLE", "RIGHT", "FAR-RIGHT");
+
+        for (int j = 0; j < 4; j++) {
+            String lado = test.get(j);
+            for (int i = 0; i < 20; i++) {
+                element.add(j, i, PlaceholderAPI.setPlaceholders(player, Survival.get().getTabConfig().getString("TABLIST." + lado + "." + (i + 1))));
+            }
+        }
+
+        element.setFooter(Utils.color(PlaceholderAPI.setPlaceholders(player, Survival.get().getTabConfig().getString("TABLIST.FOOTER"))));
+        element.setHeader(Utils.color(PlaceholderAPI.setPlaceholders(player, Survival.get().getTabConfig().getString("TABLIST.HEADER"))));
+
         return element;
     }
 }
