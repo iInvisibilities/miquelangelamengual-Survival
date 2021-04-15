cba896d5c8599d4df871720ea883eea949eef50b
Miquel Angel <hulkrojo2@gmail.com>
2021-04-15 12:30:47 +0200

    Added custom quit message

---
+++ src/main/java/es/hulk/survival/listeners/QuitListener.java
@@ -0,0 +1,22 @@
+package es.hulk.survival.listeners;
+
+import es.hulk.survival.Survival;
+import es.hulk.survival.utils.FileConfig;
+import es.hulk.survival.utils.Utils;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class QuitListener implements Listener {
+
+    private final FileConfig mainConfig = Survival.get().getMainConfig();
+
+    @EventHandler
+    public void QuitEvent(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        event.setQuitMessage(Utils.color(PlaceholderAPI.setPlaceholders(player, mainConfig.getString("PLAYER-QUIT-MESSAGE").replaceAll("%player%", player.getDisplayName()))));
+    }
+
+}
