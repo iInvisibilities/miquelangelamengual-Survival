b22e9e2b9e3a00619e44c5ecc9d24941cbc3f035
Miquel Angel <hulkrojo2@gmail.com>
2021-04-24 00:17:49 +0200

    Added command to see coords of your Bed

---
+++ src/main/java/es/hulk/survival/command/CamaCoordCommand.java
@@ -0,0 +1,22 @@
+package es.hulk.survival.command;
+
+import dev.risas.panda.files.FileConfig;
+import es.hulk.survival.Survival;
+import es.hulk.survival.utils.command.BaseCommand;
+import es.hulk.survival.utils.command.Command;
+import es.hulk.survival.utils.command.CommandArgs;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.entity.Player;
+
+public class CamaCoordCommand extends BaseCommand {
+
+    private FileConfig mainConfig = Survival.get().getMainConfig();
+
+    @Command(name = "camacoords")
+    @Override
+    public void onCommand(CommandArgs command) {
+        Player player = command.getPlayer();
+
+        player.sendMessage(PlaceholderAPI.setPlaceholders(player, mainConfig.getString("CAMA-COORDS-COMMAND")));
+    }
+}
