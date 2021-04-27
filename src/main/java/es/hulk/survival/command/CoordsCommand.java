3065bbcbaa1ff69943d116b49903c0ee76651990
Miquel Angel <hulkrojo2@gmail.com>
2021-04-27 11:02:32 +0200

    dont know the changes

--- src/main/java/es/hulk/survival/command/CoordsCommand.java
+++ src/main/java/es/hulk/survival/command/CoordsCommand.java
@@ -2,11 +2,13 @@ package es.hulk.survival.command;
 
 import dev.risas.panda.files.FileConfig;
 import es.hulk.survival.Survival;
+import es.hulk.survival.utils.Utils;
 import es.hulk.survival.utils.command.BaseCommand;
 import es.hulk.survival.utils.command.Command;
 import es.hulk.survival.utils.command.CommandArgs;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 public class CoordsCommand extends BaseCommand {
@@ -19,15 +21,29 @@ public class CoordsCommand extends BaseCommand {
         Player player = command.getPlayer();
         String[] args = command.getArgs();
 
-        if (args.length < 0) {
+        if (args.length == 0) {
             for (String s : mainConfig.getStringList("COORDS.NO-ARGS-COMMAND")) {
                 player.sendMessage(PlaceholderAPI.setPlaceholders(player, s));
             }
         }
         if (args.length > 0) {
-            Player target = Bukkit.getPlayer(args[0]);
-            for (String s : mainConfig.getStringList("COORDS.OTHER-PLAYER-COORDS")) {
-                player.sendMessage(PlaceholderAPI.setPlaceholders(player, s.replaceAll("{target}", String.valueOf(target))));
+            OfflinePlayer pTo = Bukkit.getServer().getPlayer(args[0]);
+
+            if (pTo == null) {
+                pTo = Bukkit.getServer().getOfflinePlayer(args[0]);
+            }
+
+            if (pTo == null || !pTo.hasPlayedBefore()) {
+                player.sendMessage(mainConfig.getString("COORDS.PLAYER-NO-EXIST").replaceAll("%target_player%", String.valueOf(pTo.getName())));
+            } else {
+                if (pTo.isOnline()) {
+                    for (String s : mainConfig.getStringList("COORDS.OTHER-PLAYER-COORDS")) {
+                        player.sendMessage(PlaceholderAPI.setPlaceholders(player, s.replaceAll("%target_player%", String.valueOf(pTo.getName()))));
+                    }
+                } else {
+                    player.sendMessage(Utils.color(mainConfig.getString("COORDS.PLAYER-NOT-FOUND").
+                            replaceAll("%target_player%", String.valueOf(pTo.getName()))));
+                }
             }
         }
     }
