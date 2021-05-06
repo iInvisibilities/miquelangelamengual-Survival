b97a28fe8dacce575752fc4b102e895265460f7a
Miquel Angel <hulkrojo2@gmail.com>
2021-05-06 20:12:35 +0200

    Added new ChatFormat

---
+++ src/main/java/es/hulk/survival/listeners/ChatListener.java
@@ -0,0 +1,27 @@
+package es.hulk.survival.listeners;
+
+import dev.risas.panda.files.FileConfig;
+import es.hulk.survival.Survival;
+import es.hulk.survival.utils.Utils;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class ChatListener implements Listener {
+
+    private FileConfig mainConfig = Survival.get().getMainConfig();
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+
+        if (mainConfig.getBoolean("CHAT.ENABLED")) {
+            event.setFormat(Utils.color(PlaceholderAPI.setPlaceholders(player, mainConfig.getString("CHAT.FORMAT")
+                    .replaceAll("%player%", player.getDisplayName())
+                    .replace("%message%", "%2$s"))));
+        }
+    }
+
+}
