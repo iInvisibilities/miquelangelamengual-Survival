08b7f3fea69f95377081a3c7320fc81ede08c1f7
Miquel Angel <hulkrojo2@gmail.com>
2021-05-06 20:12:57 +0200

    Added broadcast when player leave the server

--- src/main/java/es/hulk/survival/listeners/QuitListener.java
+++ src/main/java/es/hulk/survival/listeners/QuitListener.java
@@ -1,13 +1,23 @@
 package es.hulk.survival.listeners;
 
+import dev.risas.panda.files.FileConfig;
+import es.hulk.survival.Survival;
+import es.hulk.survival.utils.Utils;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class QuitListener implements Listener {
 
+    private FileConfig mainConfig = Survival.get().getMainConfig();
+
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
-        event.setQuitMessage(null);
+        Player player = event.getPlayer();
+
+        event.setQuitMessage(Utils.color(PlaceholderAPI.setPlaceholders(player, mainConfig.getString("BROADCAST.QUIT").replaceAll("%player%", player.getDisplayName()))));
     }
+
 }
