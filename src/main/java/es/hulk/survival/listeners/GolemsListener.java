beae4ebec3afe1de388a1d3f74b2c53f65ed2d92
Miquel Angel <hulkrojo2@gmail.com>
2021-06-24 01:02:31 +0200

    Added Poppy drop toggleable from iron golems

---
+++ src/main/java/es/hulk/survival/listeners/GolemsListener.java
@@ -0,0 +1,24 @@
+package es.hulk.survival.listeners;
+
+import es.hulk.survival.Survival;
+import es.hulk.survival.utils.FileConfig;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+public class GolemsListener implements Listener {
+
+    private FileConfig mainConfig = Survival.get().getMainConfig();
+
+    @EventHandler
+    public void onGolemDie(EntityDeathEvent event) {
+        if (mainConfig.getBoolean("POPPY-DROP")) {
+            if (event.getEntity().getType() == EntityType.IRON_GOLEM) {
+                event.getDrops().removeIf(is -> is.getType() == Material.POPPY);
+            }
+        }
+    }
+
+}
