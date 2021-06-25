ea446624ea5387ef67551f6285f38d5b87c0302e
Miquel Angel <hulkrojo2@gmail.com>
2021-06-25 19:45:58 +0200

    Added random number to boost gunpowder drops

--- src/main/java/es/hulk/survival/listeners/GolemsListener.java
+++ src/main/java/es/hulk/survival/listeners/GolemsListener.java
@@ -7,16 +7,25 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class GolemsListener implements Listener {
 
     private FileConfig mainConfig = Survival.get().getMainConfig();
+    private final int MAX_DROPS = 30;
+    private final int MIN_DROPS = 1;
 
     @EventHandler
     public void onGolemDie(EntityDeathEvent event) {
-        if (!mainConfig.getBoolean("POPPY-DROP")) {
+        if (!mainConfig.getBoolean("ENTITIES.POPPY-DROP")) {
             if (event.getEntity().getType() == EntityType.IRON_GOLEM) {
                 event.getDrops().removeIf(is -> is.getType() == Material.POPPY);
+            }
+        }
+        if (mainConfig.getBoolean("ENTITIES.GUNPOWDER-BOOST")) {
+            if (event.getEntity().getType() == EntityType.CREEPER) {
+                int r = (int) (Math.random() * (MAX_DROPS - MIN_DROPS)) + MIN_DROPS;
+                event.getDrops().add(new ItemStack(Material.GUNPOWDER, r));
             }
         }
     }
