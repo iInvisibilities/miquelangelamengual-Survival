3ae849616328f7637edfb7fe06adf08a7dfd3228
Miquel Angel <hulkrojo2@gmail.com>
2021-07-04 09:39:12 +0200

    Class for getting coords and world from OfflinePlayers

---
+++ src/main/java/es/hulk/survival/utils/Location/OfflinePlayerLocation.java
@@ -0,0 +1,41 @@
+package es.hulk.survival.utils.Location;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+public class OfflinePlayerLocation {
+
+    private static String getWorldName(Player player) {
+        return LocationUtils.getWorld(player);
+    }
+
+    public static String getWorld(OfflinePlayer offlinePlayer) {
+        Player player = offlinePlayer.getPlayer();
+        switch (getWorldName(player)) {
+            case "world":
+                return "Overworld";
+            case "world_nether":
+                return "Nether";
+            case "world_the_end":
+                return "The End";
+            default:
+                return "No World Found";
+        }
+    }
+
+    public static String coordinateX(OfflinePlayer offlinePlayer) {
+        Player player = offlinePlayer.getPlayer();
+        return String.valueOf(player.getLocation().getX());
+    }
+
+    public static String coordinateY(OfflinePlayer offlinePlayer) {
+        Player player = offlinePlayer.getPlayer();
+        return String.valueOf(player.getLocation().getY());
+    }
+
+    public static String coordinateZ(OfflinePlayer offlinePlayer) {
+        Player player = offlinePlayer.getPlayer();
+        return String.valueOf(player.getLocation().getZ());
+    }
+
+}
