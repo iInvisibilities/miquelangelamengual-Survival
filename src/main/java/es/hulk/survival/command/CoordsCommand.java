28c67388fc474548447ee0beab67c73cdd741fd2
Miquel Angel <hulkrojo2@gmail.com>
2021-07-04 09:17:23 +0200

    Refactor and Removed use of PlaceholderAPI placeholders

--- src/main/java/es/hulk/survival/command/CoordsCommand.java
+++ src/main/java/es/hulk/survival/command/CoordsCommand.java
@@ -16,6 +16,7 @@ public class CoordsCommand extends BaseCommand {
     private FileConfig mainConfig = Survival.get().getMainConfig();
 
     @Command(name = "coords")
+    @SuppressWarnings("deprecation")
     @Override
     public void onCommand(CommandArgs command) {
         Player player = command.getPlayer();
@@ -23,24 +24,26 @@ public class CoordsCommand extends BaseCommand {
 
         if (args.length == 0) {
             for (String s : mainConfig.getStringList("COORDS.NO-ARGS-COMMAND")) {
-                player.sendMessage(PlaceholderAPI.setPlaceholders(player, s));
+                String x = String.valueOf(player.getLocation().getX());
+                String y = String.valueOf(player.getLocation().getY());
+                String z = String.valueOf(player.getLocation().getZ());
+                player.sendMessage(PlaceholderAPI.setPlaceholders(player, s).replaceAll("<x-coord>", x).replaceAll("<y-coord>", y).replaceAll("<z-coord>", z));
             }
         }
         if (args.length > 0) {
-            OfflinePlayer pTo = Bukkit.getServer().getPlayer(args[0]);
+            OfflinePlayer offlinePlayer = Bukkit.getServer().getPlayer(args[0]);
 
-            if (pTo == null || !pTo.hasPlayedBefore()) {
-                pTo = Bukkit.getServer().getOfflinePlayer(args[0]);
-                player.sendMessage(mainConfig.getString("COORDS.PLAYER-NO-EXIST").replaceAll("%target_player%", String.valueOf(pTo.getName())));
-            } else {
-                if (pTo.isOnline()) {
-                    for (String s : mainConfig.getStringList("COORDS.OTHER-PLAYER-COORDS")) {
-                        player.sendMessage(PlaceholderAPI.setPlaceholders(player, s.replaceAll("%target_player%", String.valueOf(pTo.getName()))));
-                    }
-                } else {
-                    player.sendMessage(Utils.color(mainConfig.getString("COORDS.PLAYER-NOT-FOUND").
-                            replaceAll("%target_player%", String.valueOf(pTo.getName()))));
+            if (offlinePlayer == null || !offlinePlayer.hasPlayedBefore()) {
+                player.sendMessage(mainConfig.getString("COORDS.PLAYER-NO-EXIST").replaceAll("%target_player%", String.valueOf(offlinePlayer.getName())));
+                return;
+            }
+            if (offlinePlayer.isOnline()) {
+                for (String s : mainConfig.getStringList("COORDS.OTHER-PLAYER-COORDS")) {
+                    player.sendMessage(PlaceholderAPI.setPlaceholders(player, s.replaceAll("%target_player%", String.valueOf(offlinePlayer.getName()))));
                 }
+            } else {
+                player.sendMessage(Utils.color(mainConfig.getString("COORDS.PLAYER-NOT-FOUND").
+                        replaceAll("%target_player%", String.valueOf(offlinePlayer.getName()))));
             }
         }
     }
