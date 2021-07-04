af44f2b86633f63ce6c9da6eae534432fbf7a1f9
Miquel Angel <hulkrojo2@gmail.com>
2021-07-04 09:17:04 +0200

    Added something stupid

--- src/main/java/es/hulk/survival/listeners/BedListener.java
+++ src/main/java/es/hulk/survival/listeners/BedListener.java
@@ -1,6 +1,7 @@
 package es.hulk.survival.listeners;
 
 import es.hulk.survival.Survival;
+import es.hulk.survival.utils.FileConfig;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,19 +10,27 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class BedListener implements Listener {
 
+    private final FileConfig mainConfig = Survival.get().getMainConfig();
+
     @EventHandler
     public void onPlayerJoinBedEvent(PlayerBedEnterEvent event) {
         Player player = event.getPlayer();
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                if (player.isSleeping()) {
-                    player.getWorld().setTime(0L);
-                    player.getWorld().setStorm(false);
-                    player.getWorld().setThundering(false);
-                }
-            }
-        }.runTaskLater(Survival.get(), 100L);
-    }
 
+        if (!mainConfig.getBoolean("BED-INSTANT-SLEEP")) {
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if (player.isSleeping()) {
+                        player.getWorld().setTime(0L);
+                        player.getWorld().setStorm(false);
+                        player.getWorld().setThundering(false);
+                    }
+                }
+            }.runTaskLater(Survival.get(), 100L);
+        } else if (player.isSleeping()) {
+            player.getWorld().setTime(0L);
+            player.getWorld().setStorm(false);
+            player.getWorld().setThundering(false);
+        }
+    }
 }
