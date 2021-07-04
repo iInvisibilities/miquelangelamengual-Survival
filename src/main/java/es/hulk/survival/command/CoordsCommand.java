169d7b6d4c04ea4b20a1578d23f5ac92bfeed539
Miquel Angel <hulkrojo2@gmail.com>
2021-07-04 09:38:29 +0200

    Completely removed use of placeholderapi placeholders

--- src/main/java/es/hulk/survival/command/CoordsCommand.java
+++ src/main/java/es/hulk/survival/command/CoordsCommand.java
@@ -2,6 +2,8 @@ package es.hulk.survival.command;
 
 import es.hulk.survival.utils.FileConfig;
 import es.hulk.survival.Survival;
+import es.hulk.survival.utils.Location.OfflinePlayerLocation;
+import es.hulk.survival.utils.Location.PlayerLocation;
 import es.hulk.survival.utils.Utils;
 import es.hulk.survival.utils.command.BaseCommand;
 import es.hulk.survival.utils.command.Command;
@@ -24,22 +26,30 @@ public class CoordsCommand extends BaseCommand {
 
         if (args.length == 0) {
             for (String s : mainConfig.getStringList("COORDS.NO-ARGS-COMMAND")) {
-                String x = String.valueOf(player.getLocation().getX());
-                String y = String.valueOf(player.getLocation().getY());
-                String z = String.valueOf(player.getLocation().getZ());
-                player.sendMessage(PlaceholderAPI.setPlaceholders(player, s).replaceAll("<x-coord>", x).replaceAll("<y-coord>", y).replaceAll("<z-coord>", z));
+                player.sendMessage(PlaceholderAPI.setPlaceholders(player, s)
+                        .replaceAll("<x-coord>", PlayerLocation.coordinateX(player))
+                        .replaceAll("<y-coord>", PlayerLocation.coordinateY(player))
+                        .replaceAll("<z-coord>", PlayerLocation.coordinateZ(player))
+                        .replaceAll("<world>", PlayerLocation.getWorld(player)));
             }
         }
+
         if (args.length > 0) {
             OfflinePlayer offlinePlayer = Bukkit.getServer().getPlayer(args[0]);
 
             if (offlinePlayer == null || !offlinePlayer.hasPlayedBefore()) {
-                player.sendMessage(mainConfig.getString("COORDS.PLAYER-NO-EXIST").replaceAll("%target_player%", String.valueOf(offlinePlayer.getName())));
+                player.sendMessage(mainConfig.getString("COORDS.PLAYER-NO-EXIST")
+                        .replaceAll("<target_player>", String.valueOf(offlinePlayer.getName())));
                 return;
             }
             if (offlinePlayer.isOnline()) {
                 for (String s : mainConfig.getStringList("COORDS.OTHER-PLAYER-COORDS")) {
-                    player.sendMessage(PlaceholderAPI.setPlaceholders(player, s.replaceAll("%target_player%", String.valueOf(offlinePlayer.getName()))));
+                    player.sendMessage(PlaceholderAPI.setPlaceholders(player, s
+                            .replaceAll("<target_player>", String.valueOf(offlinePlayer.getName()))
+                            .replaceAll("<x-target-player>", OfflinePlayerLocation.coordinateX(offlinePlayer))
+                            .replaceAll("<y-target-player>", OfflinePlayerLocation.coordinateY(offlinePlayer))
+                            .replaceAll("<z-target-player>", OfflinePlayerLocation.coordinateZ(offlinePlayer))
+                            .replaceAll("<target-world>", OfflinePlayerLocation.getWorld(offlinePlayer))));
                 }
             } else {
                 player.sendMessage(Utils.color(mainConfig.getString("COORDS.PLAYER-NOT-FOUND").
