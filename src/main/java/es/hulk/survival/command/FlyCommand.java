260d23966789f855641e2e7438e971565544b83c
Miquel Angel <hulkrojo2@gmail.com>
2021-07-04 15:09:50 +0200

    Fixed FlyCommand

--- src/main/java/es/hulk/survival/command/FlyCommand.java
+++ src/main/java/es/hulk/survival/command/FlyCommand.java
@@ -4,12 +4,15 @@ import es.hulk.survival.Survival;
 import es.hulk.survival.utils.CC;
 import es.hulk.survival.utils.FileConfig;
 import es.hulk.survival.utils.command.BaseCommand;
+import es.hulk.survival.utils.command.Command;
 import es.hulk.survival.utils.command.CommandArgs;
 import org.bukkit.entity.Player;
 
 public class FlyCommand extends BaseCommand {
 
     private final FileConfig mainConfig = Survival.get().getMainConfig();
+
+    @Command(name = "fly", permission = "survival.command.fly")
 
     @Override
     public void onCommand(CommandArgs command) {
