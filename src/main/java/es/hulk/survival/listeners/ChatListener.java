a3a7d9c237b9a583049b910013d27b0e50f1ca9b
Miquel Angel <hulkrojo2@gmail.com>
2021-07-06 17:04:07 +0200

    Removed placeholderapi dependency

--- src/main/java/es/hulk/survival/listeners/ChatListener.java
+++ src/main/java/es/hulk/survival/listeners/ChatListener.java
@@ -1,9 +1,9 @@
 package es.hulk.survival.listeners;
 
 import es.hulk.survival.Survival;
+import es.hulk.survival.rank.LuckPermsAPI;
 import es.hulk.survival.utils.FileConfig;
 import es.hulk.survival.utils.Utils;
-import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,9 +18,11 @@ public class ChatListener implements Listener {
         Player player = event.getPlayer();
 
         if (mainConfig.getBoolean("CHAT.ENABLED")) {
-                event.setFormat(Utils.color(PlaceholderAPI.setPlaceholders(player, mainConfig.getString("CHAT.FORMAT")
-                        .replaceAll("%player%", player.getDisplayName())
-                        .replace("%message%", "%2$s"))));
+            event.setFormat(Utils.color(mainConfig.getString("CHAT.FORMAT")
+                    .replaceAll("<rank-suffix>", LuckPermsAPI.getSuffix(player))
+                    .replaceAll("<rank-prefix>", LuckPermsAPI.getPrefix(player))
+                    .replaceAll("%player%", player.getDisplayName())
+                    .replace("%message%", "%2$s")));
         }
     }
 }
