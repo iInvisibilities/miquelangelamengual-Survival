0b775761161cc1ca7db40e9bf28e6884929489c5
Miquel Angel <hulkrojo2@gmail.com>
2021-07-06 17:06:56 +0200

    Added method to get player ping

--- src/main/java/es/hulk/survival/utils/Utils.java
+++ src/main/java/es/hulk/survival/utils/Utils.java
@@ -54,4 +54,18 @@ public class Utils {
         }
         return players;
     }
+
+    public static int getPing(Player player) {
+        String v = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
+        if (!player.getClass().getName().equals("org.bukkit.craftbukkit." + v + ".entity.CraftPlayer")) { //compatibility with some plugins
+            player = Bukkit.getPlayer(player.getUniqueId()); //cast to org.bukkit.entity.Player
+        }
+        try {
+            int ping = player.getPing();
+            return ping;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 }
