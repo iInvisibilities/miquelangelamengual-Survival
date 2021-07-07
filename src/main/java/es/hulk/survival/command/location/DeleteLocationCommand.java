8491199bc93a7c066b898d81f0f7c3fae030ab85
Miquel Angel <hulkrojo2@gmail.com>
2021-07-07 22:14:21 +0200

    Added command to delete warps

---
+++ src/main/java/es/hulk/survival/command/location/DeleteLocationCommand.java
@@ -0,0 +1,11 @@
+package es.hulk.survival.command.location;
+
+import es.hulk.survival.utils.command.BaseCommand;
+import es.hulk.survival.utils.command.CommandArgs;
+
+public class DeleteLocationCommand extends BaseCommand {
+    @Override
+    public void onCommand(CommandArgs command) {
+
+    }
+}
