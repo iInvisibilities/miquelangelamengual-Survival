29c79821a299340ea2e35ddd42651b486e879d01
Miquel Angel <hulkrojo2@gmail.com>
2021-07-07 22:16:07 +0200

    Added command to delete warps

--- src/main/java/es/hulk/survival/command/location/DeleteLocationCommand.java
+++ src/main/java/es/hulk/survival/command/location/DeleteLocationCommand.java
@@ -1,11 +1,31 @@
 package es.hulk.survival.command.location;
 
+import es.hulk.survival.Survival;
+import es.hulk.survival.managers.WarpManager;
+import es.hulk.survival.utils.Utils;
 import es.hulk.survival.utils.command.BaseCommand;
+import es.hulk.survival.utils.command.Command;
 import es.hulk.survival.utils.command.CommandArgs;
+import org.bukkit.entity.Player;
 
 public class DeleteLocationCommand extends BaseCommand {
+
+    @Command(name = "deletelocation")
     @Override
     public void onCommand(CommandArgs command) {
+        Player player = command.getPlayer();
+        String[] args = command.getArgs();
 
+        WarpManager warpManager = Survival.get().getWarpManager();
+
+        if (args.length == 0) {
+            player.sendMessage(Utils.color("&cUsage: /deletelocation <name>"));
+        }
+
+        if (args.length > 0) {
+            player.sendMessage(Utils.color("&cLocation for " + args[0] + " has been succesfuly removed"));
+            warpManager.deleteWarpByName(args[0]);
+            warpManager.saveWarps();
+        }
     }
 }
