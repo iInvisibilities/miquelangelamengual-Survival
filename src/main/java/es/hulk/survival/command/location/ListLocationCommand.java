01bdd315fdc9e7adea96cd1549db2c0791f5319e
Miquel Angel <hulkrojo2@gmail.com>
2021-07-08 10:11:56 +0200

    Used Arraylist names from WarpManager to get a list of names

--- src/main/java/es/hulk/survival/command/location/ListLocationCommand.java
+++ src/main/java/es/hulk/survival/command/location/ListLocationCommand.java
@@ -1,22 +1,24 @@
 package es.hulk.survival.command.location;
 
+import com.sun.corba.se.impl.logging.UtilSystemException;
 import es.hulk.survival.Survival;
+import es.hulk.survival.managers.warp.WarpManager;
+import es.hulk.survival.utils.Utils;
 import es.hulk.survival.utils.command.BaseCommand;
+import es.hulk.survival.utils.command.Command;
 import es.hulk.survival.utils.command.CommandArgs;
 import org.bukkit.entity.Player;
 
-import java.util.List;
+import java.util.Collections;
 
 public class ListLocationCommand extends BaseCommand {
+
+    WarpManager warpManager = Survival.get().getWarpManager();
+
+    @Command(name = "location.list")
     @Override
     public void onCommand(CommandArgs command) {
         Player player = command.getPlayer();
-        String aux = "";
-        List<String> warpName = Survival.get().getConfig().getStringList("WARP.LIST");
-
-        for (int i = 0; i < warpName.size(); i++) {
-            aux = warpName.get(i);
-        }
-        player.sendMessage(aux);
+        player.sendMessage(Utils.color("&aAvailable Warps&7: &f" + Collections.singletonList(warpManager.getWarps())));
     }
 }
