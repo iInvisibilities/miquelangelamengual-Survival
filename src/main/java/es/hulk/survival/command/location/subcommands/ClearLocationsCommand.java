ecc604fe5c2ff1b3e175dff04147cc0c7302fb49
Miquel Angel <hulkrojo2@gmail.com>
2021-07-08 10:24:20 +0200

    Added command to clear locations.

---
+++ src/main/java/es/hulk/survival/command/location/subcommands/ClearLocationsCommand.java
@@ -0,0 +1,23 @@
+package es.hulk.survival.command.location.subcommands;
+
+import es.hulk.survival.Survival;
+import es.hulk.survival.managers.warp.WarpManager;
+import es.hulk.survival.utils.Utils;
+import es.hulk.survival.utils.command.BaseCommand;
+import es.hulk.survival.utils.command.Command;
+import es.hulk.survival.utils.command.CommandArgs;
+import org.bukkit.command.CommandSender;
+
+public class ClearLocationsCommand extends BaseCommand {
+
+    private WarpManager warpManager = Survival.get().getWarpManager();
+
+    @Command(name = "location.clear", permission = "survival.command.clearlocations")
+    @Override
+    public void onCommand(CommandArgs command) {
+        CommandSender commandSender = command.getSender();
+
+        warpManager.clearWarp();
+        commandSender.sendMessage(Utils.color("&aSuccesfully cleared all saved locations"));
+    }
+}
