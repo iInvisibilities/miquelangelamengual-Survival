b257c97ed1f6f0ee836424853ccdf0933a1f5033
Miquel Angel <miquelangel2309@gmail.com>
2021-08-11 19:22:08 +0200

    simple things

--- src/main/java/es/hulk/survival/command/essential/KillCommand.java
+++ src/main/java/es/hulk/survival/command/essential/KillCommand.java
@@ -1,9 +1,11 @@
 package es.hulk.survival.command.essential;
 
+import es.hulk.survival.utils.UUIDs;
 import es.hulk.survival.utils.Utils;
 import es.hulk.survival.utils.command.BaseCommand;
 import es.hulk.survival.utils.command.Command;
 import es.hulk.survival.utils.command.CommandArgs;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class KillCommand extends BaseCommand {
@@ -15,5 +17,9 @@ public class KillCommand extends BaseCommand {
         Player player = command.getPlayer();
         player.setHealth(0.0D);
         player.sendMessage(Utils.color("&aTe has muerto de cancer rata"));
+        if (player.getUniqueId().equals(UUIDs.rafaUUID())) {
+            Bukkit.broadcastMessage(Utils.color("&7[&aSurvival&7] &e" + player.getDisplayName() + " &ase ha suicidado. F en el chat por el hombre caido"));
+            player.kickPlayer(Utils.color("Te has suicidado cabron"));
+        }
     }
 }
