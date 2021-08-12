6c2c6b18889cfa95e14fbb6368f9560efdcec9c7
Miquel Angel <miquelangel2309@gmail.com>
2021-08-12 19:51:33 +0200

    Updated KillCommand

--- src/main/java/es/hulk/survival/command/essential/KillCommand.java
+++ src/main/java/es/hulk/survival/command/essential/KillCommand.java
@@ -13,14 +13,14 @@ import org.bukkit.entity.Player;
 public class KillCommand extends BaseCommand {
 
     private final FileConfig messagesConfig = Survival.get().getMessagesConfig();
-    @Command(name = "mekill", aliases = "suicide")
+    @Command(name = "suicide")
 
     @Override
     public void onCommand(CommandArgs command) {
         Player player = command.getPlayer();
         player.setHealth(0.0D);
         Bukkit.broadcastMessage(Utils.color(messagesConfig.getString("KILL_COMMAND.BROADCAST")).replace("<player>", player.getDisplayName()));
-        if (player.getUniqueId().equals(UUIDs.rafaUUID()) || player.getUniqueId().equals(UUIDs.jaumeUUID())) {
+        if (player.getUniqueId().equals(UUIDs.rafaUUID()) || player.getUniqueId().equals(UUIDs.nadalUUID())) {
             player.kickPlayer(Utils.color(Utils.color(messagesConfig.getString("KILL_COMMAND.KICK")).replace("<player>", player.getDisplayName())));
             Bukkit.broadcastMessage(Utils.color(messagesConfig.getString("KILL_COMMAND.KICK-MESSAGE")).replace("<player>", player.getDisplayName()));
         }
