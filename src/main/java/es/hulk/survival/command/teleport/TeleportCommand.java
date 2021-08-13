312ce67af3b0b5e620972f311baa67127b654cc0
Miquel Angel <miquelangel2309@gmail.com>
2021-08-13 02:25:48 +0200

    Teleport Command

---
+++ src/main/java/es/hulk/survival/command/teleport/TeleportCommand.java
@@ -0,0 +1,34 @@
+package es.hulk.survival.command.teleport;
+
+import es.hulk.survival.utils.Utils;
+import es.hulk.survival.utils.command.BaseCommand;
+import es.hulk.survival.utils.command.Command;
+import es.hulk.survival.utils.command.CommandArgs;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class TeleportCommand extends BaseCommand {
+
+    @Command(name = "teleport", aliases = "tp")
+
+    @Override
+    public void onCommand(CommandArgs command) {
+        Player player = command.getPlayer();
+        String[] args = command.getArgs();
+
+        if (args.length < 1) {
+            player.sendMessage(Utils.color("&cUsage: /" + command.getLabel() + " <player>"));
+            return;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+
+        if (target == null) {
+            player.sendMessage(Utils.color("&cPlayer not found"));
+            return;
+        }
+
+        player.teleport(target.getLocation());
+        player.sendMessage(Utils.color(Utils.PREFIX + "&aTe has teletransportado a &e" + target.getDisplayName()));
+    }
+}
