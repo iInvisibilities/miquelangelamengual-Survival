230527da25a082ba69132c7169503d5489e1ca84
Miquel Angel <miquelangel2309@gmail.com>
2021-08-13 02:25:39 +0200

    Command to tp all players to your position

---
+++ src/main/java/es/hulk/survival/command/teleport/TeleportAllCommand.java
@@ -0,0 +1,22 @@
+package es.hulk.survival.command.teleport;
+
+import es.hulk.survival.utils.Utils;
+import es.hulk.survival.utils.command.BaseCommand;
+import es.hulk.survival.utils.command.Command;
+import es.hulk.survival.utils.command.CommandArgs;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class TeleportAllCommand extends BaseCommand {
+    @Command(name = "tpall", permission = "survival.command.tpall")
+    @Override
+    public void onCommand(CommandArgs command) {
+        Player player = command.getPlayer();
+
+        for (Player online : Bukkit.getOnlinePlayers()) {
+            online.teleport(player.getLocation());
+        }
+
+        player.sendMessage(Utils.color(Utils.PREFIX + "&aHabeis sido teletransportado a la localizacion de &e" + player.getDisplayName()));
+    }
+}
