7087ee637bbed7647b5d0d3e8adb9eef5bc6f42a
Miquel Angel <miquelangel2309@gmail.com>
2021-08-13 02:26:12 +0200

    Command to teleport to you a player

---
+++ src/main/java/es/hulk/survival/command/teleport/TeleportHereCommand.java
@@ -0,0 +1,37 @@
+package es.hulk.survival.command.teleport;
+
+import es.hulk.survival.Survival;
+import es.hulk.survival.utils.FileConfig;
+import es.hulk.survival.utils.Utils;
+import es.hulk.survival.utils.command.BaseCommand;
+import es.hulk.survival.utils.command.Command;
+import es.hulk.survival.utils.command.CommandArgs;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class TeleportHereCommand extends BaseCommand {
+
+    private final FileConfig messagesConfig = Survival.get().getMessagesConfig();
+    @Command(name = "teleporthere", aliases = {"tphere", "s"})
+    @Override
+    public void onCommand(CommandArgs command) {
+        Player player = command.getPlayer();
+        String[] args = command.getArgs();
+
+        if (args.length < 1) {
+            player.sendMessage(Utils.color("&cUsage: /" + command.getLabel() + " <player>"));
+            return;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+
+        if (target == null) {
+            player.sendMessage(Utils.color("&cPlayer '" + args[0] + "' not found."));
+            return;
+        }
+
+        target.teleport(player.getLocation());
+        player.sendMessage(Utils.color(Utils.PREFIX + "&aHas teletransportado a &e" + target.getDisplayName() + " &aa tu posicion."));
+        target.sendMessage(Utils.color(Utils.PREFIX + "&aHas sido teletransportado a &e" + player.getDisplayName()) + "&a.");
+    }
+}
