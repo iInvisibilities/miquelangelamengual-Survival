c250acc7c79234b4f7857057604553ade22d6d0c
Miquel Angel <miquelangel2309@gmail.com>
2021-08-13 02:25:19 +0200

    Added command to kill everyone

---
+++ src/main/java/es/hulk/survival/command/admin/KillAllCommand.java
@@ -0,0 +1,27 @@
+package es.hulk.survival.command.admin;
+
+import es.hulk.survival.Survival;
+import es.hulk.survival.utils.FileConfig;
+import es.hulk.survival.utils.UUIDs;
+import es.hulk.survival.utils.Utils;
+import es.hulk.survival.utils.command.BaseCommand;
+import es.hulk.survival.utils.command.Command;
+import es.hulk.survival.utils.command.CommandArgs;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class KillAllCommand extends BaseCommand {
+
+    private final FileConfig messagesConfig = Survival.get().getMessagesConfig();
+    @Command(name = "killall", permission = "survival.command.killall")
+
+    @Override
+    public void onCommand(CommandArgs command) {
+        Player player = command.getPlayer();
+
+        for (Player online : Bukkit.getOnlinePlayers()) {
+            online.setHealth(0.0D);
+        }
+        Bukkit.broadcastMessage(Utils.color(messagesConfig.getString("KILL_COMMAND.ALL")));
+    }
+}
