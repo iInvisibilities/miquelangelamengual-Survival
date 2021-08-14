c5a46b5c960d1869dfaaf5b563c844cdab884180
Miquel Angel <miquelangel2309@gmail.com>
2021-08-14 18:51:50 +0200

    more detailed command for SeedCommand

--- src/main/java/es/hulk/survival/command/essential/SeedCommand.java
+++ src/main/java/es/hulk/survival/command/essential/SeedCommand.java
@@ -1,5 +1,6 @@
 package es.hulk.survival.command.essential;
 
+import es.hulk.survival.utils.Utils;
 import es.hulk.survival.utils.command.BaseCommand;
 import es.hulk.survival.utils.command.Command;
 import es.hulk.survival.utils.command.CommandArgs;
@@ -14,6 +15,8 @@ public class SeedCommand extends BaseCommand {
     public void onCommand(CommandArgs command) {
         Player player = command.getPlayer();
 
-        player.sendMessage(String.valueOf(Bukkit.getWorlds().get(0).getSeed()));
+        String seed = String.valueOf(Bukkit.getWorlds().get(0).getSeed());
+
+        player.sendMessage(Utils.color(Utils.PREFIX + "&aLa seed del juego es &e" + seed));
     }
 }
