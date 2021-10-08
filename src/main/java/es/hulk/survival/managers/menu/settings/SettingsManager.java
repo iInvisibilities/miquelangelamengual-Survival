a082e5f67a6d9c3ec0ff65de78100ff9f9c25174
Miquel Angel <hulkrojo2@gmail.com>
2021-10-08 22:44:21 +0200

    Revert "Added an assert"
    This reverts commit 2b1f3d483508080e3dcbffbb97d17abe1854239a.

--- src/main/java/es/hulk/survival/managers/menu/settings/SettingsManager.java
+++ src/main/java/es/hulk/survival/managers/menu/settings/SettingsManager.java
@@ -19,7 +19,6 @@ public class SettingsManager {
         items.clear();
         ConfigurationSection section = menuConfig.getConfiguration().getConfigurationSection("SETTINGS_MENU.ITEMS");
 
-        assert section != null;
         for (String item : section.getKeys(false)) {
             String displayName = menuConfig.getString("SETTINGS_MENU.ITEMS." + item + ".DISPLAYNAME");
             List<String> description = menuConfig.getStringList("SETTINGS_MENU.ITEMS." + item + ".LORE");
