9a0cd446647bac1533d66702e4d4b9faf758653d
Miquel Angel <hulkrojo2@gmail.com>
2022-03-11 11:49:11 +0100

    added 2 more uuid to play with it

--- src/main/java/es/hulk/survival/utils/PlayerUUID.java
+++ src/main/java/es/hulk/survival/utils/PlayerUUID.java
@@ -9,5 +9,7 @@ public class PlayerUUID {
     public static UUID RAFA = UUID.fromString("b7be8875-a5c4-4854-a1db-c68626373c5c");
     public static UUID JAUME = UUID.fromString("b3acc6d0-7e46-4e6f-95ba-dd5acb58883e");
     public static UUID NADAL = UUID.fromString("e05a1ef2-95cc-48eb-b847-18ab10048e91");
+    public static UUID UNAI = UUID.fromString("d9dff839-f07e-41c1-a782-21795705fa05");
+    public static UUID HARPIO = UUID.fromString("b15a1c6f-eb27-48d0-9eb9-a5f28a308372");
 
 }
