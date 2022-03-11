08bc0b92c4c4a8faedffd13df850f8b40f1ce5d3
Miquel Angel <hulkrojo2@gmail.com>
2022-03-11 11:39:20 +0100

    added experimental ban command

---
+++ src/main/java/es/hulk/survival/command/admin/BanCommand.java
@@ -0,0 +1,30 @@
+package es.hulk.survival.command.admin;
+
+import es.hulk.survival.utils.command.BaseCommand;
+import es.hulk.survival.utils.command.Command;
+import es.hulk.survival.utils.command.CommandArgs;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.Date;
+
+import static org.bukkit.BanList.Type;
+
+public class BanCommand extends BaseCommand {
+
+    @Command(name = "ban", permission = "survival.command.ban")
+
+    @Override
+    public void onCommand(CommandArgs command) {
+        Player player = command.getPlayer();
+        String[] args = command.getArgs();
+        String reason = args[1];
+
+        Player target = Bukkit.getPlayer(args[0]);
+        assert target != null;
+        if (reason == null) {
+            reason = "Default Reason";
+        }
+        Bukkit.getBanList(Type.NAME).addBan(target.getName(), reason, new Date(), player.getName());
+    }
+}
